e7d4185633ab95047f59004eb99e9c9fa3320482
Lucas Ulfenborg <55761924+lucasulfenborg@users.noreply.github.com>
2025-03-04 09:45:33 +0100

    Added initial files

---
+++ src/Fika.java
@@ -0,0 +1,12 @@
+public class Fika {
+
+	public static void main(String[] args) {
+		Fika fika = new Fika();
+        fika.startSimulation();
+	}
+	
+	private void startSimulation() {
+		System.out.println("Test test 123");
+	}
+
+}
