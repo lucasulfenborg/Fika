dbed9b91554216ff04ad24cd0dc238d78338ffa6
Lucas Ulfenborg <55761924+lucasulfenborg@users.noreply.github.com>
2025-03-09 23:23:29 +0100

    Final adjustments

--- src/Fika.java
+++ src/Fika.java
@@ -5,9 +5,12 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class Fika {
 
 	//Global variables
-	public int simulationTime = 20;
-	public int timeScale = 10; //e.g. 10 is 10 times faster 
-	public int numberOfWorkersWorking = 0;
+	//SimulationTime and timeScale should be changed for testing scenarios
+	public int simulationTime = 20; //Duration of the simulation
+	public int timeScale = 1; //e.g. 10 is 10 times faster 
+	
+	
+	public int numberOfWorkersAtWork = 0;
     public ConcurrentLinkedQueue<Coffee> readyCoffees = new ConcurrentLinkedQueue();
     public ConcurrentLinkedQueue<Worker> workersInQueue = new ConcurrentLinkedQueue<>();
 
@@ -27,11 +30,13 @@ public class Fika {
     	Worker Nora  = new Worker("Nora");
     	
     	//Spawn the coffee machine
-    	coffeeMachine coffeeMachine = new coffeeMachine();
+    	CoffeeMachine CoffeeMachine = new CoffeeMachine();
     	
-    	//Start the threads
-    	coffeeMachine.start();
+    	//Start the threads 
+    	//Makes the coffeeMachine start brewing coffee and serving workers while there are workers
+    	CoffeeMachine.start();
     	
+    	//Make the workers start working
     	Erik.start();
     	Simon.start();
     	Adrian.start();
@@ -44,13 +49,14 @@ public class Fika {
             e.printStackTrace();
         }
 
-    	//Exit the program
+    	
+    	//After the simulation time has ended, the program will exit
         System.exit(0);
     	
     }
 
     // Method for generating a random integer
-    private int RandomNumber(int min, int max) {
+    private int generateRandomNumber(int min, int max) {
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
     }
@@ -58,49 +64,59 @@ public class Fika {
     public class Worker extends Thread {
     	String name;
         int currentEnergy;
-        int EnergyLoss;
+        int energyDepletionRate;
         
         		
         public Worker (String name) {
         	this.name = name;
-            this.currentEnergy = RandomNumber(30, 90);
-            this.EnergyLoss = RandomNumber(500, 1500);
+        	//Set the starting energy of the worker to a random number from 30-90
+            this.currentEnergy = generateRandomNumber(30, 90);
+            //Set the speed of which energy will decrease
+            this.energyDepletionRate = generateRandomNumber(500, 1500);
         }	
         
+        
+        //the run() method runs when the thread is started
         @Override 
     	public void run() {
     		try {
+    			//Increment the amount of workers at work with one
+    			//To keep track that people are at work
+    			numberOfWorkersAtWork++;
     			
-    			numberOfWorkersWorking++;
-    			
+    			//the main loop of the thread
     			while (true) {
-					Thread.sleep(this.EnergyLoss / timeScale);
+    				// Pause a given amount of time at the start of each iteration
+    				//Makes it iterate as fast as the given energy depletion rate
+					Thread.sleep(this.energyDepletionRate / timeScale);
 				    //Decrease energy by 1
 			        this.currentEnergy --;
 
-			        //Chilling in the coffee room
-					if(this.currentEnergy < 30 && workersInQueue.contains(this) ) {
-						System.out.println(this.name + " is taking a break with energy level " + this.currentEnergy);
-						
-						//Go home if no energy
-						if (this.currentEnergy <= 0) {
-							System.out.println(this.name + " is tired and went home.");
-							numberOfWorkersWorking--;
-							workersInQueue.remove(this);
-							return;
+			        synchronized (workersInQueue) {
+				        //Check if the worker is in the queue
+						if(workersInQueue.contains(this) ) {
+							System.out.println(this.name + " is taking a break with energy level " + this.currentEnergy);
+							
+							//Go home if no energy
+							if (this.currentEnergy <= 0) {
+								System.out.println(this.name + " is tired and went home.");
+								numberOfWorkersAtWork--;
+								workersInQueue.remove(this);
+								return;
+							}
 						}
-					}
-			        
-					//Enter coffee room queue if below 30 energy 
-					else if ((this.currentEnergy < 30) && (this.currentEnergy > 0)) {
-						workersInQueue.add(this);
-					}
-					
-					//Work
-					else if (!workersInQueue.contains(this)) {
-						System.out.println(this.name + " is working with energy level " + this.currentEnergy);
-					}
-    			
+				        
+						//Enter coffee room queue if below 30 energy 
+						
+						else if ((this.currentEnergy < 30) && (this.currentEnergy > 0)) {
+							workersInQueue.add(this);
+						}
+						
+						//Work
+						else if (!workersInQueue.contains(this)) {
+							System.out.println(this.name + " is working with energy level " + this.currentEnergy);
+						}
+			        }
     		}
     			}
 				
@@ -112,53 +128,60 @@ public class Fika {
     	}
     }
  
-    
-    public class coffeeMachine extends Thread{
-    	int brewingTime = (2000 - 1000) / timeScale; //in milliseconds
+    //Define the coffee machine class which mainly handles brewing and serving coffees
+    public class CoffeeMachine extends Thread{
+    	//Define brewing time and time to wait before serving workers. 
+    	//which makes it brew a coffee every 2 seconds and takes 1 second to check for/serve a customer
     	int timeBetweenServes = 1000 / timeScale;
-    	
+    	int brewingTime = (2000 - timeBetweenServes) / timeScale; 
+
+    	//This code starts when the thread is started
     	@Override 
     	public void run() {
     		try {
+    			//A loop that keeps iterating
     			while (true) {
     				
     				//Brew coffees only if there are workers
-    				if (numberOfWorkersWorking > 0) {
+    				if (numberOfWorkersAtWork > 0) {
 
 						//Each second check for people in the coffee queue
 						Thread.sleep(timeBetweenServes);
 						
-						//Check if coffee is available and if someone is in the queue
-						if ((readyCoffees.size() >= 1) && (workersInQueue.size() >= 1)) {
-							
-							//Serve a coffee
-							Coffee coffeeToDrink = readyCoffees.peek();
-							Worker personToServe = workersInQueue.peek();		
-							
-							readyCoffees.poll();					
-							personToServe.currentEnergy += coffeeToDrink.getEnergy();
-							System.out.println(personToServe.name + " enjoyed a " + coffeeToDrink.getType() + " with " + coffeeToDrink.getEnergy() + " energy");
-							
-							//Remove him from the queue
-							workersInQueue.poll();
-							
-							//Add to queue again if not 100 energy yet
-							if (personToServe.currentEnergy < 100) {
-								workersInQueue.add(personToServe);
+						synchronized (readyCoffees) {
+							//Check if coffee is available and if someone is in the queue
+							if ((readyCoffees.size() >= 1) && (workersInQueue.size() >= 1)) {
+								
+								//Serve a coffee
+								Coffee coffeeToDrink = readyCoffees.peek();
+								Worker personToServe = workersInQueue.peek();		
+								
+								readyCoffees.poll();					
+								personToServe.currentEnergy += coffeeToDrink.getEnergy();
+								System.out.println(personToServe.name + " enjoyed a " + coffeeToDrink.getClass().getSimpleName() + " with " + coffeeToDrink.getEnergy() + " energy");
+								
+								//Remove the Worker from the queue
+								workersInQueue.poll();
+								
+								//Add the worker back to the queue if it hasn't 100 energy
+								if (personToServe.currentEnergy < 100) {
+									workersInQueue.add(personToServe);
+								}
 							}
 						}
 						
-						
+						//Brew a coffee if there's under 20 coffees ready
 						if (readyCoffees.size() < 20) {
-							//Wait another second to brew a coffee
+							//Brew a coffee
 							Thread.sleep(this.brewingTime);
-							readyCoffees.add(new Coffee());
+							readyCoffees.add(createRandomCoffee());
 							
 							//Show that a coffee was added and total amount of ready coffees
 							System.out.println("Drink Created. Coffee Machine has " + readyCoffees.size() + " drinks in reserve.");
+							}
 						}
     				}
-    			}
+    			
 				
 				
 			} catch (InterruptedException e) {
@@ -167,46 +190,70 @@ public class Fika {
 			}
     	}}
     
-    public class Coffee{
-    	private String type;
+    //Definition of the coffee class. It contains an integer for the amount of energy that it will give
+    abstract class Coffee{
     	private int energy;
     	
-    	public Coffee() {
-    		String[] typesOfCoffee = {"blackCoffee", "cappuccino", "latte"};
-    		
-    		//Select a random type of coffee
-    		int indexOfCoffeeType = RandomNumber(0, 2); 
-    		this.type = typesOfCoffee[indexOfCoffeeType];
-    		
-    		//Select a random amount of energy based on coffee
-    		switch(indexOfCoffeeType) {
-    			case 0:
-    				this.energy = RandomNumber(15, 20); //blackCoffee energy range
-    				break;
-    			case 1:
-    				this.energy = RandomNumber(20, 30); //cappuccino energy range
-    				break;
-    			case 2:
-    				this.energy = RandomNumber(25, 35);//latte energy range
-    				break;
-    		}
-    	}
-    	
-    	protected String getType() {
-    		return this.type;
-    	}
-    	
+    	public Coffee(int energy) {
+            this.energy = energy;
+        }
+
+    	//Getter for accessing the energy from outside the class
     	protected int getEnergy() {
     		return this.energy;
     	}
     	
-    	
     }
+    
+    //Each type of coffee is a subtype of coffee, inheriting "energy" and setting it to a random value within its own energy range
+    class BlackCoffee extends Coffee {
+
+		public BlackCoffee() {
+			super(generateRandomNumber(15, 20)); 
+		}
+    }
+    
+    class Cappuccino extends Coffee {
+
+		public Cappuccino() {
+			super(generateRandomNumber(20, 30));
+		}
+    }
+		
+    class Latte extends Coffee {
+
+		public Latte() {
+			super(generateRandomNumber(25, 33));
+		}
+    }
+    
+    //Method that returns a random coffee
+    public Coffee createRandomCoffee() {
+        
+    	//Generate a random number from 0-2, each representing a type of coffee, and then return an instance of that coffee.
+        int coffeeType = generateRandomNumber(0, 2);  
+
+        switch (coffeeType) {
+            case 0:
+                return new BlackCoffee();  
+            case 1:
+                return new Cappuccino(); 
+            case 2:
+                return new Latte(); 
+            default:
+                return null;
+        }
+    }
+		
+
+ }
+    
+    
 
     
     
     
-}
+
 
 
 
