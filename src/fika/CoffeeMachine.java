f57fd14c4d93c1993907b08336d1196eeee39817
Lucas Ulfenborg <55761924+lucasulfenborg@users.noreply.github.com>
2025-05-08 21:30:25 +0200

    encapsulation

--- src/fika/CoffeeMachine.java
+++ src/fika/CoffeeMachine.java
@@ -10,7 +10,25 @@ public class CoffeeMachine extends Thread {
         this.timeBetweenServes = 1000 / Fika.timeScale;
         this.brewingTime = (2000 - timeBetweenServes) / Fika.timeScale;
     }
+    
+    //Method for brewing a coffee of a random type
+    private Coffee createRandomCoffee() {
+        int type = Utilities.generateRandomNumber(0, 2);
+        switch (type) {
+            case 0: return new BlackCoffee();
+            case 1: return new Cappuccino();
+            case 2: return new Latte();
+            default: return null;
+        }
+    }
+    
+    //Method for serving a coffee
+    private void serveCoffee() {
 
+    }
+    
+
+    //Run method when coffeMachine thread starts
     @Override
     public void run() {
         try {
@@ -19,6 +37,7 @@ public class CoffeeMachine extends Thread {
                     Thread.sleep(timeBetweenServes);
 
                     synchronized (resources.readyCoffees) {
+                    	//Serve coffee if there is ready coffee and there's people in the queue
                         if (!resources.readyCoffees.isEmpty() && !resources.workersInQueue.isEmpty()) {
                             Coffee coffee = resources.readyCoffees.poll();
                             Worker worker = resources.workersInQueue.poll();
@@ -34,7 +53,7 @@ public class CoffeeMachine extends Thread {
 
                     if (resources.readyCoffees.size() < 20) {
                         Thread.sleep(brewingTime);
-                        resources.readyCoffees.add(resources.createRandomCoffee());
+                        resources.readyCoffees.add(createRandomCoffee());
                         System.out.println("Drink Created. Coffee Machine has " + resources.readyCoffees.size() + " drinks.");
                     }
                 }
--- src/fika/ConcurrentResources.java
+++ src/fika/ConcurrentResources.java
@@ -5,14 +5,4 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class ConcurrentResources {
     public ConcurrentLinkedQueue<Coffee> readyCoffees = new ConcurrentLinkedQueue<>();
     public ConcurrentLinkedQueue<Worker> workersInQueue = new ConcurrentLinkedQueue<>();
-
-    public Coffee createRandomCoffee() {
-        int type = Utilities.generateRandomNumber(0, 2);
-        switch (type) {
-            case 0: return new BlackCoffee();
-            case 1: return new Cappuccino();
-            case 2: return new Latte();
-            default: return null;
-        }
-    }
 }
